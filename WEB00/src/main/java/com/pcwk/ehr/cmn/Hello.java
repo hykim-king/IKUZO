e1b3803fbee0a8aa3e96a6bfd29fd6718649c143
성준모 <72649156+MacEditor@users.noreply.github.com>
2024-06-07 16:56:52 +0900

    yaya
    yayaya

---
+++ WEB00/src/main/java/com/pcwk/ehr/cmn/Hello.java
@@ -0,0 +1,44 @@
+package com.pcwk.ehr.cmn;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Hello
+ */
+@WebServlet(description = "firstServlet", urlPatterns = { "/hello.do" })
+public class Hello extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Hello() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+    
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    	String userId = req.getParameter("user_id");
+    	String passwd = req.getParameter("passwd");
+    	
+    	System.out.println("userDDD : " + userId);
+    	System.out.println("passDDD : " + passwd);
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+}
